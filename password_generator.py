605c9ba9889df73beac6b9062e20c96e8fb66739
hemanthkumarm010 <139635029+hemanthkumarm010@users.noreply.github.com>
2023-12-10 20:15:00 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,18 +2,16 @@ import random
 import string
 
 def generate_password(length, uppercase, digits, special_chars):
-    """Generate a random password based on user preferences."""
     characters = string.ascii_lowercase
-    if uppercase:
-        characters += string.ascii_uppercase
     if digits:
         characters += string.digits
     if special_chars:
         characters += string.punctuation
+    if uppercase:
+        characters += string.ascii_uppercase
 
     if not characters:
         print("Error: Please select at least one character set.")
-        return None
 
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
@@ -21,13 +19,11 @@ def generate_password(length, uppercase, digits, special_chars):
 def main():
     print("Welcome to the Password Generator!")
 
-    # Get user preferences
     length = int(input("Enter the length of the password: "))
     uppercase = input("Include uppercase letters? (y/n): ").lower() == 'y'
     digits = input("Include digits? (y/n): ").lower() == 'y'
     special_chars = input("Include special characters? (y/n): ").lower() == 'y'
 
-    # Generate and display the password
     password = generate_password(length, uppercase, digits, special_chars)
 
     if password:
